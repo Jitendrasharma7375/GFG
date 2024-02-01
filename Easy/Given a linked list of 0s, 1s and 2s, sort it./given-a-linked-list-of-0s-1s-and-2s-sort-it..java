ef912c227c35d6d8a9468d60d344ca6af89e4913
Jitendra-Sharma <jitendra.sharma7375@gmail.com>
2024-02-01 21:08:24 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Given a linked list of 0s, 1s and 2s, sort it./given-a-linked-list-of-0s-1s-and-2s-sort-it..java
@@ -0,0 +1,131 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        
+        while(t-- > 0)
+        {
+            int n =Integer.parseInt(in.readLine().trim());
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(s[i]));
+                tail = tail.next;
+            }
+
+            head = new Solution().segregate(head);
+            printList(head, out);
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static void printList(Node head,PrintWriter out)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+    
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head)
+    {
+        // add your code here
+        int[] arr=new int[3];
+        Node cur=head;
+        
+        while(cur!=null)
+        {
+            if(cur.data==0)
+                arr[0]+=1;
+                
+            else if(cur.data==1)
+                arr[1]+=1;
+                
+            else
+                arr[2]+=1;
+                
+            cur=cur.next;
+        }
+        
+        Node curr=head;
+        
+        while(arr[0]!=0)
+        {
+            curr.data=0;
+            arr[0]--;
+            curr=curr.next;
+        }
+        
+        while(arr[1]!=0)
+        {
+            curr.data=1;
+            arr[1]--;
+            curr=curr.next;
+        }
+        
+        while(arr[2]!=0)
+        {
+            curr.data=2;
+            arr[2]--;
+            curr=curr.next;
+        }
+        
+        return head;
+    }
+}
+
+
