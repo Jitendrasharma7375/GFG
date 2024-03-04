b5903fc2018e25e67c607f98d3c4c22189637bae
Jitendra-Sharma <jitendra.sharma7375@gmail.com>
2024-03-04 23:01:49 +0530

    Updated Solution - GfG to GitHub

--- Easy/Swap the array elements/swap-the-array-elements.java
+++ Easy/Swap the array elements/swap-the-array-elements.java
@@ -38,11 +38,12 @@ class Solution
     public void swapElements(int[] arr, int n)
     {
         // Code here
-         for(int i=0;i<n-2;i++)
-        {
-            int temp=arr[i];
-            arr[i]=arr[i+2];
-            arr[i+2]=temp;
+        int i=0;
+        while( i+2<n){
+             int temp =arr[i];
+            arr[i]= arr[i+2];
+            arr[i+2]= temp;
+            i++;
         }
     }
 }
