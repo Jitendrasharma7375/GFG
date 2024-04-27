1c362d8cd58647e22b5e6a20a3dd79ecc6262f88
Jitendra-Sharma <jitendra.sharma7375@gmail.com>
2024-04-27 14:28:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().mergeSort(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        this.data = key;
+        next = null;
+    }
+} */
+
+class Solution
+{
+    //Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head)
+    {
+        // add your code here
+        Node temp=head;
+        int length=0;
+        ArrayList<Integer> list=new ArrayList<>();
+        while(temp!=null)
+        {
+            list.add(temp.data);
+            length+=1;
+            temp=temp.next;
+        }
+        Collections.sort(list);
+        temp=head;
+        for(int i=0;i<length;i++)
+        {
+            temp.data=list.get(i);
+            temp=temp.next;
+        }
+        return head;
+    }
+}
+
+
